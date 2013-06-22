35a8f74cfe1afc3fd7a2a66cde43db7bb0ded154
Greg Wang <gregwym@gmail.com>
2013-06-22 12:50:51 -0400

    Add unittest for Blog class

---
+++ test/blog.js
@@ -0,0 +1,50 @@
+var Blog = require('../lib/tumblr').Blog;
+
+module.exports = {
+  setUp: function(callback) {
+    this.blog = new Blog('musiclover261.tumblr.com', '0S8LLINIwPsMy8dFgsAyUInDAxUrKn52YXy0ez4930hwfhO3LF');
+    callback();
+  },
+  testInfo: function(test) {
+    this.blog.info(function(err, response) {
+      test.ifError(err);
+      test.ok(response);
+      test.ok(response.blog);
+      test.done();
+    });
+  },
+  testAvatar: function(test) {
+    this.blog.avatar(function(err, response) {
+      test.ifError(err);
+      test.ok(response);
+      test.ok(response.avatar_url);
+      test.done();
+    });
+  },
+  testAvatarSize: function(test) {
+    this.blog.avatar(512, function(err, response) {
+      test.ifError(err);
+      test.ok(response);
+      test.ok(response.avatar_url);
+      test.done();
+    });
+  },
+  testLikes: function(test) {
+    this.blog.likes(function(err, response) {
+      test.ifError(err);
+      test.ok(response);
+      test.ok(response.liked_posts);
+      test.ok(response.liked_count);
+      test.done();
+    });
+  },
+  testPosts: function(test) {
+    this.blog.posts(function(err, response) {
+      test.ifError(err);
+      test.ok(response);
+      test.ok(response.blog);
+      test.ok(response.posts);
+      test.done();
+    });
+  }
+};
